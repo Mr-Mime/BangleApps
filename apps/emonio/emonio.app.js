696a4b2b8863dbede3d3d2cc11e3bac35d1bee3d
Mr-Mime <37423773+Mr-Mime@users.noreply.github.com>
2022-09-19 21:39:36 +0200

    Update emonio.app.js

--- apps/emonio/emonio.app.js
+++ apps/emonio/emonio.app.js
@@ -1,4 +1,4 @@
-var file_content = require("Storage").readJSON("credentials.json", true);
+var file_content = require("Storage").readJSON("emonio-creds.json", true);
 var device_creds = file_content.devices;
 
 E.showMessage(device_creds[0].deviceUser + ":" + device_creds[0].devicePass, device_creds[0].deviceId);
